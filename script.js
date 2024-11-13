3eedbf486a7624e8c5583608bbd6783bf4c28c1e
Hammm22 <ilhamdwiarsandy61@gmail.com>
2024-11-13 19:44:07 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function showRSContent() {
+    const RSContent = document.querySelector('.isiRobloxStudio');
+    RSContent.style.display = 'flex';
+}
+function hideRSContent() {
+    const RSContent = document.querySelector('.isiRobloxStudio');
+    RSContent.style.display = 'none';
+}
+function showCPPContent() {
+    const CPPContent = document.querySelector('.isiCPP');
+    CPPContent.style.display = 'flex';
+}
+function hideCPPContent() {
+    const CPPContent = document.querySelector('.isiCPP');
+    CPPContent.style.display = 'none';
+}
+function showWebContent() {
+    const CPPContent = document.querySelector('.isiWebsite');
+    CPPContent.style.display = 'flex';
+}
+function hideWebContent() {
+    const WebContent = document.querySelector('.isiWebsite');
+    WebContent.style.display = 'none';
+}
+
